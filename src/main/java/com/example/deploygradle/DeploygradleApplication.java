97257b9f759552b262ecfaaa56c398d90148ef5e
Orest Sonich <sonich.work@gmail.com>
2023-05-23 17:22:31 +0300

    123

---
+++ src/main/java/com/example/deploygradle/DeploygradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploygradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploygradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploygradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deploygradle/DeploygradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploygradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploygradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
